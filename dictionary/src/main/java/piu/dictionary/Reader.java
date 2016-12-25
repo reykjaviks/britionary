c2147b14af6c47dec097c51fdca026fbaf5fe820
Elizabeth Berg <elizabethinposti@gmail.com>
2016-12-25 17:59:27 +0200

    add Reader class

---
+++ dictionary/src/main/java/piu/dictionary/Reader.java
@@ -0,0 +1,17 @@
+package piu.dictionary;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
+
+public class Reader {
+    
+    public String convert(String json) {  
+        //Get data from fetcher
+        //Create an object
+        //Set fields
+        //Return object
+        return "wee";
+    }
+}
