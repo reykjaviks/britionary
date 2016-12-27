7e705526981974c9613506d120de2eed61354701
Elizabeth Berg <elizabethinposti@gmail.com>
2016-12-27 18:16:16 +0200

    change variable name

--- dictionary/src/main/java/piu/dictionary/DictMain.java
+++ dictionary/src/main/java/piu/dictionary/DictMain.java
@@ -3,7 +3,7 @@ package piu.dictionary;
 public class DictMain {
     public static void main(String[] args) {
 
-        WordSearcher wsearcher = new WordSearcher();
-        wsearcher.search("Little");
+        WordSearcher searcher = new WordSearcher();
+        System.out.println(searcher.search("Little"));
     }
 }
