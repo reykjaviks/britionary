dbe0ccdbb1bc59c29f12d95645b883caf750a97c
Elizabeth Berg <elizabethinposti@gmail.com>
2016-12-27 22:23:08 +0200

    remove unnecessary line break

--- dictionary/src/main/java/piu/britionary/Main.java
+++ dictionary/src/main/java/piu/britionary/Main.java
@@ -2,7 +2,6 @@ package piu.britionary;
 
 public class Main {
     public static void main(String[] args) {
-
         Searcher searcher = new Searcher();
         System.out.println(searcher.search("Little"));
     }
