75b59c39c139cd0f0c9f146d4438327f0117cb62
Elizabeth Berg <elizabethinposti@gmail.com>
2016-12-27 18:22:24 +0200

    add SearcherTest

---
+++ dictionary/src/test/java/piu/dictionary/SearcherTest.java
@@ -0,0 +1,20 @@
+package piu.dictionary;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SearcherTest {
+    
+    public SearcherTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testSearch() {
+    }
+    
+}
