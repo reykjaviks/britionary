df38d9e85ceaf3f0c57e05b4e18f4209d09a32b3
Elizabeth Berg <elizabethinposti@gmail.com>
2016-12-27 18:12:14 +0200

    add Asker class

---
+++ dictionary/src/main/java/piu/dictionary/Asker.java
@@ -0,0 +1,22 @@
+package piu.dictionary;
+
+import java.util.Scanner;
+
+public class Asker { //Singleton?
+    
+    private Scanner reader;
+    private String word;
+    
+    public void scan() {
+        reader = new Scanner(System.in);
+        
+        try {
+            word = reader.nextLine();
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+    }
+    public String getWord() {
+        return word;
+    }
+}
