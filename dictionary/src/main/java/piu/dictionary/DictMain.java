d6218f78525b3d230cd5b90151a582eb186b9bfc
Elizabeth Berg <elizabethinposti@gmail.com>
2016-12-27 18:31:13 +0200

    remove DictMain

--- dictionary/src/main/java/piu/dictionary/DictMain.java
+++
@@ -1,9 +0,0 @@
-package piu.dictionary;
-
-public class DictMain {
-    public static void main(String[] args) {
-
-        WordSearcher searcher = new WordSearcher();
-        System.out.println(searcher.search("Little"));
-    }
-}
