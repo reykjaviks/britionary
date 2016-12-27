eab7a3ab3ddcc4d957bffc0eb2aa6eb20d2b5cbf
Elizabeth Berg <elizabethinposti@gmail.com>
2016-12-27 18:21:31 +0200

    change class name

---
+++ dictionary/src/main/java/piu/dictionary/Main.java
@@ -0,0 +1,9 @@
+package piu.dictionary;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Searcher searcher = new Searcher();
+        System.out.println(searcher.search("Little"));
+    }
+}
