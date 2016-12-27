02527510d3b779d01bc554a8cebb93dcaa6385e0
Elizabeth Berg <elizabethinposti@gmail.com>
2016-12-27 18:15:07 +0200

    add AskerTest

---
+++ dictionary/src/test/java/piu/dictionary/AskerTest.java
@@ -0,0 +1,24 @@
+package piu.dictionary;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+//Unfinished
+public class AskerTest {
+    
+    Asker asker;
+    
+    public AskerTest() {
+    }
+    
+    @Before
+    public void setUp() {
+        asker = new Asker();
+        
+    }
+
+    @Test
+    public void testScan() {
+    }
+    
+}
