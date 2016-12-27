fb87c63042a84ce67f1599a57800c789177141ff
Elizabeth Berg <elizabethinposti@gmail.com>
2016-12-27 23:04:18 +0200

    fix checkstyle errors

--- dictionary/src/main/java/piu/britionary/Fetcher.java
+++ dictionary/src/main/java/piu/britionary/Fetcher.java
@@ -11,16 +11,16 @@ public class Fetcher {
 
     private StringBuilder str;
     private String word;
-    private final String app_id;
-    private final String app_key;
+    private final String appID;
+    private final String addKey;
     
     public Fetcher() {
-        this.app_id = "19275027";
-        this.app_key = "fbf42d1294623d8ecfe2f595a193fdaa";
+        this.appID = "19275027";
+        this.addKey = "fbf42d1294623d8ecfe2f595a193fdaa";
     }
-    public Fetcher(String app_id, String app_key) {
-        this.app_id = app_id;
-        this.app_key = app_key;
+    public Fetcher(String appID, String appKey) {
+        this.appID = appID;
+        this.addKey = appKey;
     }
     //TODO: Should be private
     public String convertWord(String word) {
@@ -29,7 +29,7 @@ public class Fetcher {
         
         //Replace underscores
         for (int i = 0; i < word.length(); i++) {
-            if(word.charAt(i) == '_') {
+            if (word.charAt(i) == '_') {
                 str.setCharAt(i, ' ');
             }
         }
@@ -56,23 +56,23 @@ public class Fetcher {
     public String fetchJSON(String word) {
 
         //TODO: Raise exception
-        if (this.app_id == null || this.app_key == null
-                || this.app_id.isEmpty() || this.app_key.isEmpty()) {
+        if (this.appID == null || this.addKey == null
+                || this.appID.isEmpty() || this.addKey.isEmpty()) {
             return "";
         }
         
-        String word_id = convertWord(word);
+        String wordID = convertWord(word);
         
         //Example code from https://developer.oxforddictionaries.com/documentation#/
         String language = "en";
-        String link = "https://od-api.oxforddictionaries.com:443/api/v1/entries/" + language + "/" + word_id + "/synonyms;antonyms";
+        String link = "https://od-api.oxforddictionaries.com:443/api/v1/entries/" + language + "/" + wordID + "/synonyms;antonyms";
 
         try {
             URL url = new URL(link);
             HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
             urlConnection.setRequestProperty("Accept", "application/json");
-            urlConnection.setRequestProperty("app_id", this.app_id);
-            urlConnection.setRequestProperty("app_key", this.app_key);
+            urlConnection.setRequestProperty("app_id", this.appID);
+            urlConnection.setRequestProperty("app_key", this.addKey);
 
             // read the output from the server
             BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
