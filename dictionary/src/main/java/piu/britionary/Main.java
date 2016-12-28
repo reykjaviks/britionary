3716e06a2e22bc5e71733ab7348e9086f42df7aa
Elizabeth Berg <elizabethinposti@gmail.com>
2016-12-28 19:51:15 +0200

    add TODO

--- dictionary/src/main/java/piu/britionary/Main.java
+++ dictionary/src/main/java/piu/britionary/Main.java
@@ -3,7 +3,10 @@ package piu.britionary;
 public class Main {
 
     public static void main(String[] args) {
+        //TODO: research Singleton
         Searcher searcher = new Searcher();
         System.out.println(searcher.search("Little"));
+        
+        
     }
 }
