969ee98b85a2e4b5d6c95663d744579d9df02330
Elizabeth Berg <elizabethinposti@gmail.com>
2016-12-30 20:17:19 +0200

    add parseJSONNew methods

--- dictionary/src/main/java/britionary/logic/Fetcher.java
+++ dictionary/src/main/java/britionary/logic/Fetcher.java
@@ -53,7 +53,6 @@ public class Fetcher {
         return this.newWord;
     }
 
-    //Prototype
     public JSONArray findResults(JSONObject response) {
         if (response.has("results")) {
             return response.getJSONArray("results");
@@ -69,10 +68,53 @@ public class Fetcher {
         }
         return null;
     }
-    
+
     public JSONArray findEntries(JSONObject lexicalEntries) {
         if (lexicalEntries.has("entries")) {
             return lexicalEntries.getJSONArray("entries");
+        }
+        return null;
+    }
+
+    public JSONObject findSenses(JSONArray entries) {
+        for (int i = 0; i < entries.length(); i++) {
+            if (entries.getJSONObject(i).has("senses")) {
+                return entries.getJSONObject(i);
+            }
+        }
+        return null;
+    }
+    
+    public JSONArray findSubSenses(JSONObject senses) {
+        if (senses.has("subsenses")) {
+            return senses.getJSONArray("subsenses");
+        }
+        return null;
+    }
+
+    public JSONObject findSynonyms(JSONArray subsenses) {
+        for (int i = 0; i < subsenses.length(); i++) {
+            if (subsenses.getJSONObject(i).has("synonyms")) {
+                return subsenses.getJSONObject(i);
+            }
+        }
+        return null;
+    }
+    
+    public JSONArray findRegions(JSONObject synonyms) {
+        if (synonyms.has("regions")) {
+            return synonyms.getJSONArray("regions");
+        }
+        return null;
+    }
+
+    public String findRegionalNames(JSONArray regions) {
+        for (int i = 0; i < regions.length(); i++) {
+            if (regions.getString(i).equals("British") || 
+                    regions.getString(i).equals("Scottish") ||
+                    regions.getString(i).equals("Irish")) {
+                return regions.getString(i);
+            }
         }
         return null;
     }
@@ -90,15 +132,33 @@ public class Fetcher {
                 if (findEntries(findLexicalEntries((findResults(response)))) == null) {
                     return "Cannot find entries.";
                 } else {
-                    
+                    if (findSenses(findEntries(findLexicalEntries((findResults(response))))) == null) {
+                        return "Cannot find senses.";
+                    } else {
+                        if (findSubSenses(findSenses(findEntries(findLexicalEntries((findResults(response)))))) == null) {
+                            return "Cannot find subsenses.";
+                        } else {
+                            if (findSynonyms(findSubSenses(findSenses(findEntries(findLexicalEntries((findResults(response))))))) == null) {
+                                return "Cannot find synonyms.";
+                            } else {
+                                if (findRegions(findSynonyms(findSubSenses(findSenses(findEntries(findLexicalEntries((findResults(response)))))))) == null) {
+                                    return "Cannot find regions.";
+                                } else {
+                                    if (findRegionalNames(findRegions(findSynonyms(findSubSenses(findSenses(findEntries(findLexicalEntries((findResults(response))))))))) == null) {
+                                        return "Cannot find regional words.";
+                                    } else {
+                                        return "Region: " + findRegionalNames(findRegions(findSynonyms(findSubSenses(findSenses(findEntries(findLexicalEntries((findResults(response)))))))));
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
-
-        return "Completion.";
     }
 
-    //TODO: clean up
+    //Prototype
     public String parseJSON(String json) {
         StringBuilder str = new StringBuilder(); //TODO: change later
         JSONObject response = new JSONObject(json);
@@ -121,7 +181,6 @@ public class Fetcher {
 
         JSONArray synonyms = subsense4.getJSONArray("synonyms");
 
-        //TODO: clean up
         try {
             for (int i = 0; i < synonyms.length(); i++) {
 
