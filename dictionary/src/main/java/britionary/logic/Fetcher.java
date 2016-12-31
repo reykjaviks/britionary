23a08f5924c5eb9fff0e0c54e5401b18dbdacb0e
Elizabeth Berg <elizabethinposti@gmail.com>
2016-12-31 13:44:41 +0200

    clean up

--- dictionary/src/main/java/britionary/logic/Fetcher.java
+++ dictionary/src/main/java/britionary/logic/Fetcher.java
@@ -23,11 +23,7 @@ public class Fetcher {
         this.appKey = appKey;
     }
 
-    /*
-    TODO:
-    Should be a private method
-    Replace loops with String.replace()
-    */
+    //TODO: use String.replace()
     public String convertWord(String word) {
 
         StringBuilder str = new StringBuilder(word.toLowerCase());
@@ -57,7 +53,7 @@ public class Fetcher {
         return this.newWord;
     }
 
-    //TODO: implement further
+
     public JSONArray findResults(JSONObject response) {
         if (response.has("results")) {
             return response.getJSONArray("results");
@@ -165,104 +161,6 @@ public class Fetcher {
                 }
             }
         }
-    }
-
-    //Prototype 1.2
-    public String parseJSON(String json) {
-        StringBuilder str = new StringBuilder();
-        JSONObject response = new JSONObject(json);
-        
-        JSONArray results = response.getJSONArray("results");
-        JSONObject result = results.getJSONObject(0);
-
-        JSONArray lexicalEntries = result.getJSONArray("lexicalEntries");
-        JSONObject lexicalEntry = lexicalEntries.getJSONObject(1); //175
-
-        JSONArray entries = lexicalEntry.getJSONArray("entries");
-        JSONObject entry = entries.getJSONObject(0);
-
-        JSONArray senses = entry.getJSONArray("senses");
-        JSONObject sense = senses.getJSONObject(0);
-
-        JSONArray subsenses = sense.getJSONArray("subsenses");
-        JSONObject subsense4 = subsenses.getJSONObject(3); //300
-
-        JSONArray synonyms = subsense4.getJSONArray("synonyms");
-
-        try {
-            for (int i = 0; i < synonyms.length(); i++) {
-
-                JSONObject synonym = synonyms.getJSONObject(i);
-                JSONArray synonymRegions = synonym.getJSONArray("regions");
-
-                for (int j = 0; j < synonymRegions.length(); j++) {
-                    if (synonymRegions.getString(j).equals("British")
-                            || synonymRegions.getString(i).equals("Scottish")
-                            || synonymRegions.getString(i).equals("Irish")) {
-                        str.append(synonymRegions.getString(i) + ": "
-                                + synonym.getString("text") + "\n");
-                    }
-                }
-            }
-        } catch (Exception e) {
-            return "Cannot find synonyms for \"" + this.newWord + "\"";
-        }
-        return str.toString();
-
-        /*
-        //Prototype 1.1
- 
-        for (int i = 0; i < regions.length(); i++) {
-            if (regions.getString(i).equals("British")
-                    || regions.getString(i).equals("Scottish")
-                    || regions.getString(i).equals("Irish")) {
-                str.append(regions.getString(i) + "\n");
-            }
-        }
-
-        for (int i = 0; i < lexicalEntries.length(); i++) {
-            lexicalEntries.getJSONObject(i);
-        }
-
-        //Prototype 1.0
-        
-        JSONObject response = new JSONObject(json);
-        JSONObject metadata = response.getJSONObject("metadata");
-        
-        JSONArray results = response.getJSONArray("results");
-        JSONObject result = results.getJSONObject(0); //contains id, lang and lexical entries
-        
-        JSONArray lexicalEntries = result.getJSONArray("lexicalEntries");
-        JSONObject lexicalEntry = lexicalEntries.getJSONObject(0);
-        
-        JSONArray entries = lexicalEntry.getJSONArray("entries");
-        JSONObject entry = entries.getJSONObject(0); //contains homographNumber and senses
-
-        JSONArray senses = entry.getJSONArray("senses");
-        JSONObject sense = senses.getJSONObject(0);
-
-        JSONArray senseSynonyms = sense.getJSONArray("synonyms");
-        JSONObject senseSynonym = senseSynonyms.getJSONObject(0);
-        JSONObject senseSynonymX = senseSynonyms.getJSONObject(1);
-
-        JSONArray subsenses = sense.getJSONArray("subsenses");
-        JSONObject subsense = subsenses.getJSONObject(0);
-
-        JSONArray subSenseSynonyms = subsense.getJSONArray("synonyms");
-        
-        JSONObject subSenseSynonym = subSenseSynonyms.getJSONObject(0);
-        JSONObject subSenseSynonymX = subSenseSynonyms.getJSONObject(1);
-  
-        return  result.getString("id") + ": " 
-                + result.getString("language") + ": " 
-                + entries.length() + ": " 
-                + entry.getString("homographNumber") + ": " 
-                + subsense.getString("id") + ": " //genID_
-                + senseSynonym.getString("id") + ": " 
-                + senseSynonymX.getString("id") + ": " 
-                + subSenseSynonym.getString("id") + ": "
-                + subSenseSynonymX.getString("id");
-        */
     }
 
     public String fetchJSON(String word) {
