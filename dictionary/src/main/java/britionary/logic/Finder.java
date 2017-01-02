0d1bf852340be5359dbe3e7aa853d1236650c411
Elizabeth Berg <elizabethinposti@gmail.com>
2017-01-02 14:51:06 +0200

    implement Finder

--- dictionary/src/main/java/britionary/logic/Finder.java
+++ dictionary/src/main/java/britionary/logic/Finder.java
@@ -1,5 +1,24 @@
 package britionary.logic;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class Finder {
-    
+
+    public JSONArray findJSONArray(JSONObject object, String arrayID) {
+        if (object.has(arrayID)) {
+            return object.getJSONArray(arrayID);
+        }
+        return null;
+    }
+
+    public JSONObject findFirstJSONObject(JSONArray array, String objectID) {
+        for (int i = 0; i < array.length(); i++) {
+            if (array.getJSONObject(i).has(objectID)) {
+                
+            }
+        }
+        return null;
+    }
+
 }
