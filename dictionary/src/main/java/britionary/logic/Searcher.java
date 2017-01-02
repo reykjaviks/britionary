29a0e8017ce4034a5b6f4dcdd730c1c47f8224ed
Elizabeth Berg <elizabethinposti@gmail.com>
2017-01-02 12:31:51 +0200

    update Searcher logic

--- dictionary/src/main/java/britionary/logic/Searcher.java
+++ dictionary/src/main/java/britionary/logic/Searcher.java
@@ -5,13 +5,16 @@ public class Searcher {
 
     private Parser parser;
     private Fetcher fetcher;
+    private Converter converter;
 
     public Searcher() {
         parser = new Parser();
         fetcher = new Fetcher();
+        converter = new Converter();
     }
 
     public String search(String word) {
-        return parser.parseJSON(fetcher.fetchJSON(word));
+        converter.convert(word);
+        return parser.parseJSON(fetcher.fetchJSON(converter.getWord()));
     }
 }
