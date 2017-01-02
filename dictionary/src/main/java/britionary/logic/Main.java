a659cf9836a05f2755d60fd87bf2abdf73d7f382
Elizabeth Berg <elizabethinposti@gmail.com>
2017-01-02 13:18:21 +0200

    remove comments

--- dictionary/src/main/java/britionary/logic/Main.java
+++ dictionary/src/main/java/britionary/logic/Main.java
@@ -3,9 +3,9 @@ package britionary.logic;
 public class Main {
 
     public static void main(String[] args) {
-        //TODO: research Singleton
-        //Searcher searcher = new Searcher();
+        
         Searcher searcher = Searcher.getInstance();
         System.out.println(searcher.search("Ace"));
+        
     }
 }
