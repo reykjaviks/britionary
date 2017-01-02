beb8cb719d22633dd9751182be02c89e890fec5b
Elizabeth Berg <elizabethinposti@gmail.com>
2017-01-02 12:58:25 +0200

    implement Singleton to Searcher

--- dictionary/src/main/java/britionary/logic/Main.java
+++ dictionary/src/main/java/britionary/logic/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         //TODO: research Singleton
-        Searcher searcher = new Searcher();
+        //Searcher searcher = new Searcher();
+        Searcher searcher = Searcher.getInstance();
         System.out.println(searcher.search("Ace"));
     }
 }
--- dictionary/src/main/java/britionary/logic/Searcher.java
+++ dictionary/src/main/java/britionary/logic/Searcher.java
@@ -6,11 +6,19 @@ public class Searcher {
     private Parser parser;
     private Fetcher fetcher;
     private Converter converter;
+    private static Searcher instance = null;
 
-    public Searcher() {
+    private Searcher() {
         parser = new Parser();
         fetcher = new Fetcher();
         converter = new Converter();
+    }
+
+    public static Searcher getInstance() {
+        if (instance == null) {
+            instance = new Searcher();
+        }
+        return instance;
     }
 
     public String search(String word) {
