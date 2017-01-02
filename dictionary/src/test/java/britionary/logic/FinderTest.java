d1e7b9f651761fc1f10e2b328b9019d0680fc6dd
Elizabeth Berg <elizabethinposti@gmail.com>
2017-01-02 14:24:58 +0200

    add test class for Finder

---
+++ dictionary/src/test/java/britionary/logic/FinderTest.java
@@ -0,0 +1,20 @@
+package britionary.logic;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FinderTest {
+    
+    public FinderTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
