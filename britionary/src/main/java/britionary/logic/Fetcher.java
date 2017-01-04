009992ac26ceb52d39183fe3c50020d515e8c65c
Elizabeth Berg <elizabethinposti@gmail.com>
2017-01-04 16:44:27 +0200

    remove unused imports

--- britionary/src/main/java/britionary/logic/Fetcher.java
+++ britionary/src/main/java/britionary/logic/Fetcher.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import javax.net.ssl.HttpsURLConnection;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 public class Fetcher {
 
