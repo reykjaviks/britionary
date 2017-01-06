67a6e8173d37e2bb768071747c50e3013fc84388
Elizabeth Berg <elizabethinposti@gmail.com>
2017-01-06 16:54:56 +0200

    add RegionalWordTest

---
+++ britionary/src/test/java/britionary/logic/RegionalWordTest.java
@@ -0,0 +1,32 @@
+package britionary.logic;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class RegionalWordTest {
+    
+    public RegionalWordTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testGetRegion() {
+    }
+
+    @Test
+    public void testGetWord() {
+    }
+
+    @Test
+    public void testHashCode() {
+    }
+
+    @Test
+    public void testEquals() {
+    }
+    
+}
