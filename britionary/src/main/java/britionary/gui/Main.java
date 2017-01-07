e18ed845623cb13713b09aefbae9e74d96b84c6c
Elizabeth Berg <elizabethinposti@gmail.com>
2017-01-07 18:14:00 +0200

    remove unused import

--- britionary/src/main/java/britionary/gui/Main.java
+++ britionary/src/main/java/britionary/gui/Main.java
@@ -1,11 +1,9 @@
 package britionary.gui;
 
-import britionary.logic.*;
-
 public class Main {
 
     public static void main(String[] args) {
-        GUIBuilder.createAndShowGUI(); 
+        GUIBuilder.createAndShowGUI();
     }
     
 }
