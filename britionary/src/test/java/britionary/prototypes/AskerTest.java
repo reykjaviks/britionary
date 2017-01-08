f2948f60e0df2b24e4b4fe4543e18435044610cb
Elizabeth Berg <elizabethinposti@gmail.com>
2017-01-08 11:28:00 +0200

    move AskerTest to prototypes

--- britionary/src/test/java/britionary/prototypes/AskerTest.java
+++ britionary/src/test/java/britionary/prototypes/AskerTest.java
@@ -1,14 +1,14 @@
-package britionary.logic;
+package britionary.prototypes;
 
 import java.io.ByteArrayInputStream;
-import britionary.logic.Asker;
+import britionary.prototypes.AskerPrototype;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AskerTest {
     
-    Asker asker;
+    AskerPrototype asker;
     String data;
     
     public AskerTest() {
@@ -20,14 +20,14 @@ public class AskerTest {
 
     @Test
     public void testScanException() {
-        asker = new Asker(null);
+        asker = new AskerPrototype(null);
         asker.scan();
         assertEquals(null, asker.getWord());
     }
     @Test
     public void testScan() {
         data = "Hello World.";
-        asker = new Asker(new ByteArrayInputStream(data.getBytes()));
+        asker = new AskerPrototype(new ByteArrayInputStream(data.getBytes()));
         asker.scan();
         assertEquals("Hello World.", asker.getWord());
     }
