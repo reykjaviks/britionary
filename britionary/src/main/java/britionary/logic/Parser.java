77ee38bd5e5884f0f4bcca61d4fd36d7fa704a60
Elizabeth Berg <elizabethinposti@gmail.com>
2017-01-09 16:55:53 +0200

    cut parseJSON into parseJSONBrits and parseJSONAll

--- britionary/src/main/java/britionary/logic/Parser.java
+++ britionary/src/main/java/britionary/logic/Parser.java
@@ -15,15 +15,30 @@ public class Parser {
      * @return                  Lista löydetyistä synonyymeista
      * @throws  ParseException  Heittää poikkeuksen jos merkkijono on tyhjä
      */
-    public static String parseJSON(String json) throws ParseException {
+    public static String parseJSONBrits(String json) throws ParseException {
         JSONObject response = new JSONObject(json);
-        HashSet<RegionalWord> wordSet = Handler.handleResults(response);
-        String synonyms = "";    
+        HashSet<RegionalWord> wordSet = BritishHandler.handleResults(response);
+
+        if (wordSet.isEmpty()) {
+            throw new ParseException("No British synonyms forcat ");
+        }
+        String synonyms = "";
         for (RegionalWord word : wordSet) {
             synonyms += word.getWord() + "\n";
         }
-        if (synonyms == "") {
-            throw new ParseException("No British synonyms found.");
+        return synonyms;
+    }
+
+    public static String parseJSONAll(String json) throws ParseException {
+        JSONObject response = new JSONObject(json);
+        HashSet<RegionalWord> wordSet = Handler.handleResults(response);
+
+        if (wordSet.isEmpty()) {
+            throw new ParseException("No synonyms for ");
+        }
+        String synonyms = "";
+        for (RegionalWord word : wordSet) {
+            synonyms += word.getWord() + "\n";
         }
         return synonyms;
     }
