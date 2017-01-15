f032753dae1eb9aeb2be69a3015e32980120d9f5
Elizabeth Berg <elizabethinposti@gmail.com>
2017-01-15 14:40:14 +0200

    remove unnecessary object creation

--- britionary/src/test/java/britionary/logic/FinderTest.java
+++ britionary/src/test/java/britionary/logic/FinderTest.java
@@ -1,29 +1,17 @@
 package britionary.logic;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
-import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class FinderTest {
 
-    Finder finder;
-
-    public FinderTest() {
-    }
-
-    @Before
-    public void setUp() {
-        finder = new Finder();
-    }
-    
     @Test
     public void testFindJSONArrayResults() {
         String json = "{\n"
                 + "    \"results\": []}\n";
         JSONObject response = new JSONObject(json);
-        assertNotNull(finder.findJSONArray(response, "results"));
+        assertNotNull(Finder.findJSONArray(response, "results"));
     }
 
 }
